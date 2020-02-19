29bc7fb30ee1c47e82603b71486e3fc7243c1d83
Michael Ritsch <43600935+MichaelRitsch@users.noreply.github.com>
2020-02-19 09:38:17 +0100

    Create category_menue.php

---
+++ category_menue.php
@@ -0,0 +1,29 @@
+<?php
+
+// Create a Category menue blog entries
+
+function get_term_menue($post_type = 'post', $taxonomy = 'category'){
+	$html = '';
+	$terms = get_terms( array(
+		'taxonomy' => $taxonomy,
+		'hide_empty' => true,
+	));
+
+	//var_dump($_SERVER);
+
+	if(sizeof($terms)>0){
+		$html .= '<ul>';
+		foreach($terms as $term){
+			if(!is_wp_error($term)){
+				$term_link = get_term_link($term);
+				$active = is_tax($taxonomy, $term->slug );
+				$html .= "<li class='";
+				$html .= ($active === true) ? 'active' : '';
+				$html .= "'><a href='$term_link' title='$term->name'>$term->name</a></li>";
+			}
+		}
+		$html .= '</ul>';
+	}
+
+	return $html;
+}
